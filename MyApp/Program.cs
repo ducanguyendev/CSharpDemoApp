8ee5b2c80c0ea77296e22f90ccf2665a71bc7920
ducanguyen <duca16122005@gmail.com>
2025-03-28 18:18:08 +0700

    init project

---
+++ MyApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Xin chào mọi người nhaaaa!");
+        }
+    }
+}
